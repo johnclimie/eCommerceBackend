d92f65627a94a4c1b0b8a357471ceae05bcd7f94
John Climie <johnclimie00@gmail.com>
2022-02-02 18:33:27 -0500

    Added async

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,7 +52,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
     const tagData = await Tag.update(req.body, {
@@ -72,7 +72,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
     const tagData = await Tag.destroy({
