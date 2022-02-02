62f56c8cb58deab9981d0b80e01230a235de3c49
John Climie <johnclimie00@gmail.com>
2022-02-02 17:25:33 -0500

    Added associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreginKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
